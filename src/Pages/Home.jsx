a891007dc5ba0e7dac714552f7f611bb6228c11a
João Victor <joaovba2002@alu.ufc.br>
2022-11-10 22:37:47 -0300

    Titles

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,39 @@
 import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
 
-const moviesURL = import.meta.env.VITE_API
-const apiKey = import.meta.env.VITE_API_KEY
+// get the API access data
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
-    const [topMovies, setTopMovies] = useState([])
+    // Use State with to The Top Movies data
+    const [topMovies, setTopMovies] = useState([]);
 
+    // Function to get the top movieas data from API
     const getTopRatedMovies = async (url) => {
-        const res = await fetch(url)
-        const data = await res.json()
-        console.log(data)
+        // Fecth in the API
+        const res = await fetch(url);
+        // Convert data in JSON
+        const data = await res.json();
+        setTopMovies(data.results);
     }
 
+    // Whenever the page reloads execute this block
     useEffect(() => {
-        const topRatedUrl = `${moviesURL}top_rated?${apiKey}`
-        getTopRatedMovies(topRatedUrl)
-    }, [])
+        setInterval(() => {
+            const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+            getTopRatedMovies(topRatedUrl);
+        }, 1000);
+    }, []);
 
+    // Return some JSX with the Page Component
     return (
-        <div>
-            <h3>Home</h3>
+        <div className="container">
+            <h2 className="title">Melhores filmes</h2>
+            <div className="movies-container">
+                {topMovies.length === 0 && <p>Carregando...</p>}
+                {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+            </div>
         </div>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
-import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi'
+import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
 
 const Navbar = () => {
     return (
         <nav id="navbar">
             <h2>
-                <Link to='/'> <BiCameraMovie/> My Movies </Link>
+                <Link to='/'>
+                    <BiCameraMovie /> My Movies 
+                </Link>
             </h2>
             <form>
                 <input type="text" placeholder="Busque um filme" />
                 <button type="submit">
-                    <BiSearchAlt2/>
+                    <BiSearchAlt2 />
                 </button>
             </form>
 
